153f2c340c0d484dee3bd2c0909b171493d6dc72
rakr0819 <raman.krauchanka@netcracker.com>
2024-11-11 12:32:08 +0300

    feat: update track api

--- src/controllers/track.controller.ts
+++ src/controllers/track.controller.ts
@@ -12,26 +12,107 @@ import { IdDto } from '../dto/common.dto';
 import { CreateTrackDto, TrackDto, UpdateTrackDto } from '../dto/track.dto';
 import { TrackService } from '../services/track.service';
 import { MessageHelper } from '../helpers/message.helper';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { SwaggerExamples } from '../helpers/swagger.helper';
+import { StatusCodes } from 'http-status-codes';
 
 @Controller('track')
 export class TrackController {
   constructor(private trackService: TrackService) {}
 
+  @ApiOperation({
+    summary: 'Get tracks list',
+    description: 'Gets all library tracks list',
+  })
+  @ApiOkResponse({
+    description: 'Successful operation',
+    example: SwaggerExamples.TRACKS,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Access token is missing or invalid',
+  })
   @Get()
   getAll() {
     return this.trackService.getAll();
   }
 
+  @ApiOperation({
+    summary: 'Get single track by id',
+    description: 'Get single track by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'string($uuid)',
+    required: true,
+  })
+  @ApiOkResponse({
+    description: 'Successful operation',
+    example: SwaggerExamples.TRACK,
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request. trackId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Access token is missing or invalid',
+  })
+  @ApiNotFoundResponse({
+    description: 'Track was not found',
+  })
   @Get(':id')
   async get(@Param() { id }: IdDto): Promise<TrackDto> {
     return this.trackService.get(id);
   }
 
+  @ApiOperation({
+    summary: 'Add new track',
+    description: 'Add new track information',
+  })
+  @ApiCreatedResponse({
+    description: 'Track is created',
+    example: SwaggerExamples.TRACK,
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request. body does not contain required fields',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Access token is missing or invalid',
+  })
   @Post()
   create(@Body() body: CreateTrackDto) {
     return this.trackService.create(body);
   }
 
+  @ApiOperation({
+    summary: 'Update track information',
+    description: 'Update track information by UUID',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'string($uuid)',
+    required: true,
+  })
+  @ApiOkResponse({
+    description: 'The track has been updated.',
+    example: SwaggerExamples.TRACK,
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request. trackId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Access token is missing or invalid',
+  })
+  @ApiNotFoundResponse({
+    description: 'Track was not found',
+  })
   @Put(':id')
   update(
     @Param() { id }: IdDto,
@@ -40,8 +121,29 @@ export class TrackController {
     return this.trackService.update(id, body);
   }
 
+  @ApiOperation({
+    summary: 'Delete track',
+    description: 'Delete track from library',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'string($uuid)',
+    required: true,
+  })
+  @ApiNoContentResponse({
+    description: 'Deleted successfully',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request. trackId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Access token is missing or invalid',
+  })
+  @ApiNotFoundResponse({
+    description: 'Track was not found',
+  })
   @Delete(':id')
-  @HttpCode(204)
+  @HttpCode(StatusCodes.NO_CONTENT)
   async delete(@Param() { id }: IdDto): Promise<string> {
     await this.trackService.delete(id);
     return MessageHelper.deleteSuccessfully('Track');
--- src/helpers/swagger.helper.ts
+++ src/helpers/swagger.helper.ts
@@ -25,4 +25,20 @@ export enum SwaggerExamples {
     '  "year": 1991,\n' +
     '  "artistId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"\n' +
     '}',
+  TRACKS = '[\n' +
+    '  {\n' +
+    '    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",\n' +
+    '    "name": "The Show Must Go On",\n' +
+    '    "artistId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",\n' +
+    '    "albumId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",\n' +
+    '    "duration": 262\n' +
+    '  }\n' +
+    ']',
+  TRACK = '{\n' +
+    '  "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",\n' +
+    '  "name": "The Show Must Go On",\n' +
+    '  "artistId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",\n' +
+    '  "albumId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",\n' +
+    '  "duration": 262\n' +
+    '}',
 }
